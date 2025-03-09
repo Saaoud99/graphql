04df774664c03ce70cf47f4ddbf5b4520842862d
Saaoud99 <saaoud.abdelhadi@gmail.com>
2025-03-09 14:14:07 +0000

    adds

--- graph.js
+++ graph.js
@@ -1,5 +1,5 @@
 const Url = "https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql";
-const jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5OCIsImlhdCI6MTczOTg5NTU3OSwiaXAiOiIxMC4xLjguMSwgMTcyLjE4LjAuMiIsImV4cCI6MTczOTk4MTk3OSwiaHR0cHM6Ly9oYXN1cmEuaW8vand0L2NsYWltcyI6eyJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiXSwieC1oYXN1cmEtY2FtcHVzZXMiOiJ7fSIsIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS11c2VyLWlkIjoiOTgiLCJ4LWhhc3VyYS10b2tlbi1pZCI6ImUxYTNlNzIxLTZkOGUtNDU4MC04YWY3LWRmYTcyZTFlYjNlYSJ9fQ.5u8TraEVzxSMZmxil1Dbo8fKvq1kWazJ_FioNWpsyto";
+const jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5OCIsImlhdCI6MTc0MTUyOTEwOCwiaXAiOiIxOTYuMjAwLjE1OS4xMTQsIDE3Mi4xOC4wLjIiLCJleHAiOjE3NDE2MTU1MDgsImh0dHBzOi8vaGFzdXJhLmlvL2p3dC9jbGFpbXMiOnsieC1oYXN1cmEtYWxsb3dlZC1yb2xlcyI6WyJ1c2VyIl0sIngtaGFzdXJhLWNhbXB1c2VzIjoie30iLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwieC1oYXN1cmEtdXNlci1pZCI6Ijk4IiwieC1oYXN1cmEtdG9rZW4taWQiOiIxMWM3OGE4NC00OTNlLTRjMTMtYTJjZi1lNWIxYzI0MzdiOTQifX0.gXKGteOCo5JT5YUtLw_OZJQUk3ytvOPTAZI_5u-eLEI";
 
 async function login(username, password) {
     let base = btoa(`${username}:${password}`)
@@ -67,7 +67,16 @@ async function getdata(query) {
     return data;
 }
 
-console.log(getdata());
-console.log(getdata());
-console.log(getdata());
+getdata(query.query1).then((data)=>{
+    console.log(data.data.user);
+    
+})
+getdata(query.query2).then((data)=>{
+    console.log(data.data.transaction);
+    
+})
+getdata(query.query3).then((data)=>{
+    console.log(data.data.user);
+    
+})
 
