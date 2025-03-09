3d8ce60fe95164c47d2b89aa0f821fe7659df6dc
Saaoud99 <saaoud.abdelhadi@gmail.com>
2025-03-09 13:51:54 +0000

    hhh

--- graph.js
+++ graph.js
@@ -1,24 +1,19 @@
 const Url = "https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql";
 const jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5OCIsImlhdCI6MTczOTg5NTU3OSwiaXAiOiIxMC4xLjguMSwgMTcyLjE4LjAuMiIsImV4cCI6MTczOTk4MTk3OSwiaHR0cHM6Ly9oYXN1cmEuaW8vand0L2NsYWltcyI6eyJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiXSwieC1oYXN1cmEtY2FtcHVzZXMiOiJ7fSIsIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS11c2VyLWlkIjoiOTgiLCJ4LWhhc3VyYS10b2tlbi1pZCI6ImUxYTNlNzIxLTZkOGUtNDU4MC04YWY3LWRmYTcyZTFlYjNlYSJ9fQ.5u8TraEVzxSMZmxil1Dbo8fKvq1kWazJ_FioNWpsyto";
 
-// async function login(username, password) {
-//     let base = btoa(`${username}:${password}`)
-//     const response = await fetch(Url, {
-//         method: 'POST',
-//         headers: {
-//             "Authorization": `Basic ${base}`,
-//         },
-//     })
-//     const data = await response.json()
-//     console.log(data);
+async function login(username, password) {
+    let base = btoa(`${username}:${password}`)
+    const response = await fetch(Url, {
+        method: 'POST',
+        headers: {
+            "Authorization": `Basic ${base}`,
+        },
+    })
+    const data = await response.json()
+    console.log(data);
 
-//     // if (data.error) {
-//     //     console.log(data.error);
-//     // }else{
-//     //     console.log(data);
-//     // }
 
-// }
+}
 let query = {
 
     query1: `
@@ -59,32 +54,20 @@ let query = {
                 }
             `
 }
-async function getdata() {
+async function getdata(query) {
     const response = await fetch(Url, {
         method: 'POST',
         headers: { "Authorization": `Bearer ${jwt}` },
         body: JSON.stringify({
-            query: `
-                    {
-                    user{
-                        firstName
-                        lastName
-                    
-                    }
-
-                    
-                    }
-
-            `
+            query: query
         })
     })
-    console.log("hdjhgdn");
 
-    const data = await response.json()
-
-    console.log(data.data);
-
-
+    const data = await response.json();
+    return data;
 }
 
-getdata()
+console.log(getdata());
+console.log(getdata());
+console.log(getdata());
+
